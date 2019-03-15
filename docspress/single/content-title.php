4c8f6da5eceeb51d3a812c809a646a5b4f52174a
vladlent <plafon89@gmail.com>
2019-03-16 00:53:49 +0300

    remove post views

--- docspress/single/content-title.php
+++ docspress/single/content-title.php
@@ -20,7 +20,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<?php the_title( '<h1 class="entry-title" itemprop="headline">', '</h1>' ); ?>
 
 	<div class="vlt-post-meta">
-		<span><i class="fas fa-bolt"></i><a href="<?php the_permalink(); ?>"><?php echo docs_get_post_views( get_the_ID() ); ?><?php docs_get_post_views( get_the_ID() ) == 1 ? esc_html_e( ' view', '@@textdomain' ) : esc_html_e( ' views', '@@textdomain' ); ?></a></span>
 		<span><i class="fas fa-calendar-plus"></i><a href="<?php the_permalink(); ?>"><time datetime="<?php the_modified_date( 'c' ); ?>"><?php echo the_modified_date(); ?></time></a></span>
 	</div>
 	<!-- /.vlt-post-meta -->
